9ced18d1e50592248336158d044f2e8da707aca8
Salomanuel <salomanue.aux+code@gmail.com>
2016-08-31 12:03:24 +0200

    add tweeting capabilities with .tweet method

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -5,5 +5,13 @@ class MicroBlogger
 
 	def initialize
 		puts "Initializing MicroBlogger"
+		@client = JumpstartAuth.twitter
+	end
+
+	def tweet(message)
+		@client.update(message)
 	end
 end
+
+blogger = MicroBlogger.new
+blogger.tweet("MicroBlogger Initialized")
