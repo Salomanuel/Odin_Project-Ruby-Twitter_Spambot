301772f1c53739607cfd11c0a7832890c9a0a486
Salomanuel <salomanue.aux+code@gmail.com>
2016-09-01 12:37:03 +0200

    interface interfaced

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -6,6 +6,8 @@ class MicroBlogger
 	def initialize
 		puts "Initializing MicroBlogger"
 		@client = JumpstartAuth.twitter			#OAuth AUTHENTICATION
+		puts "Welcome to the JSL Twitter Client!"
+		run
 	end
 
 	def tweet(message)
@@ -21,11 +23,21 @@ class MicroBlogger
 		else
 			@client.update(message)
 		end
-		
+	end
+
+	def run
+		puts "press Q to exit"
+		command = gets.chomp.downcase
+		case command
+		when "q" then puts "Goodbye"
+		else
+			puts "what the hell does \"#{command}\" means"
+			run
+		end
 	end
 end
 
 blogger = MicroBlogger.new
 #blogger.tweet("MicroBlogger Initialized")
-blogger.tweet("this tweet is way longer than the usual 140 characters. Let's see what will happen after I try to send it. I'm very curious about it. My bloody ear hurts because of this stupid otitis, I can't wait for it to finish")
+#blogger.tweet("this tweet is way longer than the usual 140 characters. Let's see what will happen after I try to send it. I'm very curious about it. My bloody ear hurts because of this stupid otitis, I can't wait for it to finish")
 #blogger.tweet("are you still working?")
